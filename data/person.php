a160e6f83698c86c36119c7b1d6d2e7e283601f1
GHIVARI MGP <fahripardiansyah@gmail.com>
2024-12-03 10:56:56 +0700

    MGP

--- data/person.php
+++ data/person.php
@@ -14,7 +14,28 @@ class Person
     var string $address;
     var string $country = "Indonesia"; // Default Value
 
+    const JUMLAH_KAKI = 2;
+
+    function __construct(
+        string $name,
+        string $address,   
+        int $age,
+        string $country = "Indonesia", // Default Value
+    )
+    {
+        $this->name = $name; 
+        $this->age = $age; 
+        $this->address = $address;
+        $this->country = $country;
+    }
     function tahunLahir() {
-        return 2024 - $this->age;
+        return 2024 - $this->age; 
+        // variable $this digunakan untuk objek
+    }
+
+    function jumlahSandal() {
+        return self::JUMLAH_KAKI / 2;   
+        // variable self digunakan untuk class
+        // self:: digunakan untuk mengakses constant     
     }
 }
--- tes.php
+++ tes.php
@@ -1,20 +1,33 @@
 <?php
 require_once "data/Person.php";
 
-$person = new Person();
-$person->name = "Syafiq";
-$person->age = 20;
-$person->address = "solo";
+// new Person () adalah class
+// $person adalah objek
+// $person->name = "Syafiq";
+// $person->age = 20;
+// $person->address = "solo";
+$person = new Person( "Syafiq", "solo", 20, "Indonesia" );
 var_dump($person);
 echo "Tahun kelahiran $person->name adalah tahun " . $person->tahunLahir() . PHP_EOL;
+echo "Jumlah Kaki $person->name adalah " . Person::JUMLAH_KAKI . PHP_EOL;
+echo "$person->name membutuhkan " . $person->jumlahSandal() . " pasang" . PHP_EOL;
+echo "$person->name Tinggal di Negara $person->country" . PHP_EOL;
+echo PHP_EOL;
 
-$fulan = new Person();
-$fulan->name = "Ghivari";
-$fulan->age = 18;
-$fulan->address = "Cibitung";
+
+// $fulan = new Person();
+// $fulan->name = "Ghivari";
+// $fulan->age = 18;
+// $fulan->address = "Cibitung";
+$fulan = new Person( "Ghivari", "Cibitung", 18, "Indonesia" ); 
 var_dump($fulan);
-echo "Tahun kelahiran $fulan->name adalah tahun " . $person->tahunLahir() . PHP_EOL;
+echo "Tahun kelahiran $fulan->name adalah tahun " . $fulan->tahunLahir() . PHP_EOL;
+echo "Jumlah Kaki $fulan->name adalah " . Person::JUMLAH_KAKI . PHP_EOL;
+echo "$fulan->name membutuhkan " . $fulan->jumlahSandal() . " pasang" . PHP_EOL;
+echo "$fulan->name Tinggal di Negara $fulan->country" . PHP_EOL;
+echo PHP_EOL;
 
 
-// $fulanah = new Person();
-// var_dump($fulanah);
+$fulanah = new Person("Fulanah", "Cikarang", 18);
+var_dump($fulanah);
+echo "$fulanah->name tinggal di Negara $fulanah->country" . PHP_EOL;
